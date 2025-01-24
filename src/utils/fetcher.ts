c3c2c8b1ba675df1cb584d1580a749178773e4e2
wcw <344078971@qq.com>
2025-01-24 21:13:15 +0800

    feat: add NEXT_PUBLIC_API_URL to deploy workflow and log URL in fetcher function

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,10 +1,16 @@
 export const fetcher = (input: string | URL | Request, init?: RequestInit) => {
   if (typeof input === "string") {
     let url = input;
+    console.log(
+      "process.env.NEXT_PUBLIC_API_URL",
+      process.env.NEXT_PUBLIC_API_URL,
+    );
 
     if (process.env.NEXT_PUBLIC_API_URL) {
       url = `${process.env.NEXT_PUBLIC_API_URL}${url}`;
     }
+
+    console.log("url", url);
 
     return fetch(url, init).then((res) => res.json());
   }
