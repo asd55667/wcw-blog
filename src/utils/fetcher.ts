a17bc22c5f921af9121e3fb06faea18a3e74f920
wcw <344078971@qq.com>
2025-01-24 17:56:58 +0800

    feat: enhance fetcher function to support GitHub Pages and improve URL handling

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,8 +1,14 @@
+import pkg from "@/../package.json";
+
 export const fetcher = (input: string | URL | Request, init?: RequestInit) => {
   if (typeof input === "string") {
-    const url = process.env.NEXT_PUBLIC_API_URL
-      ? process.env.NEXT_PUBLIC_API_URL + input
-      : input;
+    let url = input;
+    if (process.env.NEXT_PUBLIC_API_URL) {
+      url = `${process.env.NEXT_PUBLIC_API_URL}${url}`;
+    } else if (process.env.PLATFORM === "github pages") {
+      url = `${pkg.name}${url}`;
+    }
+
     return fetch(url, init).then((res) => res.json());
   }
 
