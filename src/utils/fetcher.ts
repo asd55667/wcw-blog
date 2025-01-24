745d0dece33330fcfc7c29132b9b5e6d49d7402b
wcw <344078971@qq.com>
2025-01-24 21:07:20 +0800

    fix: remove unused GitHub Pages URL handling from fetcher function

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,16 +1,10 @@
-import pkg from "@/../package.json";
-
 export const fetcher = (input: string | URL | Request, init?: RequestInit) => {
   if (typeof input === "string") {
     let url = input;
 
     if (process.env.NEXT_PUBLIC_API_URL) {
       url = `${process.env.NEXT_PUBLIC_API_URL}${url}`;
-    } else if (process.env.PLATFORM === "github pages") {
-      url = `${pkg.name}${url}`;
     }
-
-    console.log("url", url);
 
     return fetch(url, init).then((res) => res.json());
   }
