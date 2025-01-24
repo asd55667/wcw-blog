a00ad60ed734c7d556626875eda6f035c8bb3a33
wcw <344078971@qq.com>
2025-01-24 17:41:49 +0800

    fix: improve fetcher function to handle undefined API URL gracefully

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,9 +1,9 @@
 export const fetcher = (input: string | URL | Request, init?: RequestInit) => {
   if (typeof input === "string") {
-    console.log(process.env.NEXT_PUBLIC_API_URL + input);
-    return fetch(process.env.NEXT_PUBLIC_API_URL + input, init).then((res) =>
-      res.json(),
-    );
+    const url = process.env.NEXT_PUBLIC_API_URL
+      ? process.env.NEXT_PUBLIC_API_URL + input
+      : input;
+    return fetch(url, init).then((res) => res.json());
   }
 
   return fetch(input, init).then((res) => res.json());
